8133a377a8e6f5962ba10f5ddfdab107f18bfd41
BillyBacker <38096723+BillyBacker@users.noreply.github.com>
2020-08-26 12:28:38 +0700

    General fix

--- Profund_Week3_Project1/Source.cpp
+++ Profund_Week3_Project1/Source.cpp
@@ -11,20 +11,15 @@ int len(const char In[256]) {
 	return count;
 }
 
-bool any_word(char In[256], char str[2048]) {
+bool any(char In[256], char str[2048]) {
 	int check = 0;
-	//printf("%s %s %d %d\n", In, str, len(In), len(str));
 	for (int sector = 0; sector < len(str); sector++) {
-		//printf("Sector : %d\n", sector);
 		for (int Char = 0; Char < len(In); Char++) {
-			//printf("char : %d\n", Char);
-			//printf("%c : %c\n", In[Char], str[Char+sector]);
 			if (check == len(In)) {
 				return true;
 			}
 			if (In[Char] == str[Char + sector]) {
 				check++;
-				//printf("found 1 -----------\n");
 			}
 			else {
 				check = 0;
@@ -42,7 +37,7 @@ int main() {
 	scanf("%s", &a);
 	printf("Enter Search key word (Max 255 charactors) : ");
 	scanf("%s", &b);
-	if (any_word(b, a)) {
+	if (any(b, a)) {
 		printf("Yes");
 	}
 	else {
